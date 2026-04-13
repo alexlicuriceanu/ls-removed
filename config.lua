a510bcc2eabc2719ab572815765882cb36931c54
Alex Licuriceanu <alex.licuriceanu@gmail.com>
2026-04-14 00:11:58 +0300

    chore: config values + water xml

--- config.lua
+++ config.lua
@@ -2,13 +2,13 @@ config = {}
 
 config.disable_ambient_sounds = true   -- disable ambient sounds like traffic, sirens. note: also disables Cayo Perico ambient sounds
 
-config.custom_water_name = 'ls_cayo_water'   -- set to nil to disable, or set to a key in config.custom_water to load a custom water file
+config.custom_water_name = 'ls_water'   -- set to nil to disable, or set to a key in config.custom_water to load a custom water file
 config.custom_water = {
     ['ls_water'] = {
-        resource_name = GetCurrentResourceName(), path = 'data/water.xml'
+        resource_name = GetCurrentResourceName(), path = 'data/water.xml', deep_ocean_scaler = 1.0, global_water_type = 0
     },
     ['ls_cayo_water'] = {
-        resource_name = GetCurrentResourceName(), path = 'data/ls_cayo_water.xml', deep_ocean_scaler = 0.0, global_water_type = 1
+        resource_name = GetCurrentResourceName(), path = 'data/water_ls_cayo.xml', deep_ocean_scaler = 0.0, global_water_type = 1
     },
 
     -- example custom water config, you can use water from another resource or with different settings
